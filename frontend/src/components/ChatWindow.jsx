39516632867863fe84839a0bfe62652cc4b9e0d4
KaranSingh3112 <karansingh311206@gmail.com>
2026-06-16 20:29:36 +0530

    Created frontend for Chat window

--- frontend/src/components/ChatWindow.jsx
+++ frontend/src/components/ChatWindow.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import "../styles/ChatWindow.css"
+import Chat from './Chat'
 
 export default function ChatWindow() {
   return (
-    <div>
-      Chat window
+    <div className='chatWindow'>
+      <div className="navbar">
+        <span>K-GPT <i class="fa-solid fa-angle-down"></i></span>
+        <div className="userIconDiv">
+          <span className='userIcon'><i class="fa-solid fa-user"></i></span>
+        </div>
+      </div>
+
+      <Chat />
+
+      <div className="chatInput">
+        <div className="inputBox">
+          <input type="text" placeholder='Ask anything...'/>
+          <div id='submit'><i class="fa-solid fa-arrow-up"></i></div>
+        </div>
+        <p className="info">
+          K-GPT can make mistakes. Check important info. See Cookie Prefrences
+        </p>
+      </div>
     </div>
   )
 }
