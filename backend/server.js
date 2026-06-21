75f9ad734f935651ff3d99349204abd88eae7298
KaranSingh3112 <karansingh311206@gmail.com>
2026-06-21 20:58:45 +0530

    Fixed some issues

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import express from "express";
 import cors from "cors";
 import mongoose from 'mongoose';
 import chatRoute from "./routes/chat.js"
+import authRoute from "./routes/authRoutes.js"
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api", chatRoute)
+app.use("/", authRoute)
 
 app.listen(8080, () => {
   console.log("App listens on port 8080");
